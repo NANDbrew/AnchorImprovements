70a3f14eca82866bb8a7d00fa41ae30e36171ecb
NANDbrew <zortman@gmail.com>
2024-03-01 17:13:57 -0600

    Update Version

--- Main.cs
+++ Main.cs
@@ -12,7 +12,7 @@ namespace AnchorRework
     {
         public const string GUID = "com.nandbrew.anchorimprovements";
         public const string NAME = "Anchor Improvements";
-        public const string VERSION = "2.0.0.2";
+        public const string VERSION = "2.0.0.3";
 
         internal static Main instance;
 
